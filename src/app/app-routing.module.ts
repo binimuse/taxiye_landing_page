7a65616133228b0439fbfb2a2075eeaf35892378
natnaelelnetech <natnael.m@elnetech.com>
2020-05-12 22:06:58 +0300

    navbar and coursol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home_page/home/home.component';
+import { NavBarComponent } from './container/nav-bar/nav-bar/nav-bar/nav-bar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    component : HomeComponent
+},
+{
+  path : "header",
+  component : NavBarComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home_page/home/home.component';
+import { NavBarComponent } from './container/nav-bar/nav-bar/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
