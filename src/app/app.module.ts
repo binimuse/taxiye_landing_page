d73580f04e464ef535a2bdf94776248bbd758561
natnaelelnetech <natnael.m@elnetech.com>
2020-05-15 09:53:44 +0300

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home_page/home/home.component';
 import { NavBarComponent } from './container/nav-bar/nav-bar/nav-bar/nav-bar.component';
+import { DriverRegisterComponent } from './driver-register/driver-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    DriverRegisterComponent
   ],
   imports: [
     BrowserModule,
